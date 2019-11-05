100158e6d010b300b74b5acee354fbb57c59d70c
Tom Nijhof <tom.nijhof@cytosmart.com>
2019-11-05 20:46:36 +0100

    format with black

--- 0process-data/download_data/main.py
+++ 0process-data/download_data/main.py
@@ -2,15 +2,15 @@ import urllib.request
 import shutil
 import os
 
+
 def downloadData(
-    locationData: str="temp.zip",
-    url: str =r"https://ndownloader.figshare.com/files/18501824?private_link=f41918598b1ff5116825",
-    
+    locationData: str = "temp.zip",
+    url: str = r"https://ndownloader.figshare.com/files/18501824?private_link=f41918598b1ff5116825",
 ):
-    '''
+    """
     locationData: The name of the zip file. 
     url: This is the download link. Pasting this link in your browser should tricker a download.
-    '''
+    """
     # Check if path goes to zipfile
     if not locationData.endswith(".zip"):
         raise ValueError(f"The file path should go to a .zip file. Not {locationData}")
@@ -18,14 +18,14 @@ def downloadData(
     # Check if file already exist
     if os.path.exists(locationData):
         return
-    
+
     # Make the path if is doesn't already exist
     path = os.path.split(locationData)[0]
     print("path", path)
     if not os.path.exists(path):
         print("make path", path)
         os.makedirs(path)
-    
+
     print("Start downloading the data")
     # Download the zip from from the download link
     with urllib.request.urlopen(url) as response, open(locationData, "wb") as out_file:

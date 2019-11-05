e2ec9fb9860ab4d089f03556f764d66b7659e3f7
Tom Nijhof <tom.nijhof@cytosmart.com>
2019-11-05 22:12:51 +0100

    add parser for url

--- 1.download-data/downloadData.py
+++ 1.download-data/downloadData.py
@@ -10,7 +10,14 @@ parser.add_argument(
     dest="downloadLocation",
     help="The account name of the blobStorage where the validation data is stored",
 )
+parser.add_argument(
+    "--downloadUrl",
+    type=str,
+    default=r"https://ndownloader.figshare.com/files/18501824?private_link=f41918598b1ff5116825",
+    dest="downloadUrl",
+    help="The account name of the blobStorage where the validation data is stored",
+)
 
 args = parser.parse_args()
 
-downloadData(args.downloadLocation)
+downloadData(args.downloadLocation, args.downloadUrl)

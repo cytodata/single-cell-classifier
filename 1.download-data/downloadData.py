eba66ea69d06e94ad99c95cfbf2121aeefcaf5b7
gwaygenomics <gregory.way@gmail.com>
2019-11-10 11:13:59 -0500

    add unzip argument
    also fixing the default download path

--- 1.download-data/downloadData.py
+++ 1.download-data/downloadData.py
@@ -1,18 +1,30 @@
 import argparse
 import os
 
-from scripts import downloadData
+from scripts import downloadData, unzipData
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "--downloadLocation",
     type=str,
-    default=os.path.join("..", "data", "cytodata_2019_orig_challenge_data.zip"),
+    default=os.path.join("data", "cytodata_2019_orig_challenge_data.zip"),
     dest="downloadLocation",
     help="The local directory where the downloaded data will be stored",
 )
 
+parser.add_argument(
+    "--unzip",
+    action="store_true",
+    help="include if the downloaded file should be unzipped",
+)
+
 args = parser.parse_args()
+downloadLocation = args.downloadLocation
+unzip = args.unzip
+
 downloadUrl = r"https://ndownloader.figshare.com/files/18501824"
-downloadData(args.downloadLocation, downloadUrl)
+downloadData(downloadLocation, downloadUrl)
+
+if unzip:
+    unzipData(downloadLocation)

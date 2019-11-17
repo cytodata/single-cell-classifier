5617c563bf3513c5be1dff6b6d47d78b6ca30f67
wagenrace <tom.nijhof@cytosmart.com>
2019-11-17 10:38:51 -0500

    add pipline process to analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -3,3 +3,6 @@
 # Step 1: Download the data from figshare: https://doi.org/10.6084/m9.figshare.10247531.v1
 cd 1.download-data
 ./download-pipeline.sh
+
+cd ../2.process-data
+./process-data-pipeline.sh

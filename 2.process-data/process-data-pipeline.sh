1002f4f5716f87d7156b55ec05e85eba72d36c66
gwaygenomics <gregory.way@gmail.com>
2019-11-17 11:15:26 -0500

    execute jupyter notebooks to process single cell profiles

--- 2.process-data/process-data-pipeline.sh
+++ 2.process-data/process-data-pipeline.sh
@@ -1,1 +1,19 @@
-python split_training_test.py
+#!/bin/bash
+#
+# Take the data downloaed in 1.download-data and process it for input into downstream
+# analysis modules, including machine learning applications.
+set -e
+
+# Step 1: Randomly split training data into training and test sets
+jupyter nbconvert --to=html \
+  --FilesWriter.build_directory=scripts/html \
+  --ExecutePreprocessor.kernel_name=python \
+  --ExecutePreprocessor.timeout=10000000 \
+  --execute 0.random-test-split.ipynb
+
+# Step 2: Process single cell morphology profiles in the training and test sets
+jupyter nbconvert --to=html \
+  --FilesWriter.build_directory=scripts/html \
+  --ExecutePreprocessor.kernel_name=python \
+  --ExecutePreprocessor.timeout=10000000 \
+  --execute 1.process-ebimage-features.ipynb

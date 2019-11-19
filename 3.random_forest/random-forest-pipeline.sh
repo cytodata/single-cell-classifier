3e8fc6e1479b9f0bad2ae81ff9e82b3dbec5cc1f
Tom Nijhof <nijhof.dns@gmail.com>
2019-11-19 20:18:26 +0100

    add pipeline step to folder

---
+++ 3.random_forest/random-forest-pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Process the data using methods centered around random forest classifiers
+set -e
+
+# Run the notebook
+jupyter nbconvert --to=html \
+  --FilesWriter.build_directory=scripts/html \
+  --ExecutePreprocessor.kernel_name=python \
+  --ExecutePreprocessor.timeout=10000000 \
+  --execute random_forest.ipynb

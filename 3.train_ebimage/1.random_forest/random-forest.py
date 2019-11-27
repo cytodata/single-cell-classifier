9eec7ca9b9e1e4bdd7646715d73d520afb8292c1
wagenrace <tom@Toms-unicorn-tracker.localdomain>
2019-11-27 16:17:27 +0100

    reduce the total amount of code

---
+++ 3.train_ebimage/1.random_forest/random-forest.py
@@ -0,0 +1,41 @@
+# %% Initialization 
+import sys
+from pathlib import Path
+
+basefolder_loc = Path(__file__).parents[2]
+
+import os
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+sys.path.append(str(basefolder_loc))
+from utils import create_metrics
+
+data_dir = os.path.join(basefolder_loc, "2.process-data", "data")
+
+
+def load_data(file_name="train_processed.tsv.gz"):
+    file_loc = os.path.join(data_dir, file_name)
+    data = pd.read_csv(file_loc, sep="\t")
+    X = np.array(data.drop(columns=["cell_code", "cell_id", "plate", "well", "target"]))
+    Y = np.array(data.target)
+    return X, Y
+
+
+# %% Training model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+RFclf = RandomForestClassifier(n_estimators=1000, max_depth=15, random_state=None)
+X, Y = load_data("train_processed.tsv.gz")
+RFclf.fit(X, Y)
+
+# %% Validation of model
+valX, valY = load_data("test_processed.tsv.gz")
+prediction = RFclf.predict(valX)
+
+create_metrics(
+    prediction, valY, os.path.join(os.path.join(os.path.dirname(__file__)), "results")
+)

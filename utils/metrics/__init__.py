8d0eb9de6fe7914003bae129c70a2b35e6523761
wagenrace <tom@Toms-unicorn-tracker.localdomain>
2019-11-27 16:17:03 +0100

    add seperate step for validation

---
+++ utils/metrics/__init__.py
@@ -0,0 +1,2 @@
+from .create_confusion_matrix import create_confusion_matrix
+from .add_score import add_score2excel
---
+++ utils/metrics/add_score.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def add_score2excel(score: float, filename: str):
+    try:
+        resultDf = pd.read_excel(filename, index_col=0)
+    except:
+        resultDf = pd.DataFrame(columns=["score"])
+
+    newData = {"score": score}
+    resultDf = resultDf.append(newData, ignore_index=True)
+
+    mean = resultDf.score.mean()
+    std = resultDf.score.std()
+    n = len(resultDf.score)
+
+    print(f"score is {mean} ± {2 * std} (2σ) with n = {n}")
+    resultDf.to_excel(filename)

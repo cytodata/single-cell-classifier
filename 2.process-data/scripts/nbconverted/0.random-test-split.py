13c199c10b22406ea42b6ca72aedad5480d97500
wagenrace <nijhof.dns@gmail.com>
2019-12-01 13:37:27 +0100

    remove black make over of 2.process-data

--- 2.process-data/scripts/nbconverted/0.random-test-split.py
+++ 2.process-data/scripts/nbconverted/0.random-test-split.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # Randomly Split Test Partition from Training
-#
+# 
 # Randomly split out 10% of the training data into a testing partion to assess model performance during model development.
 
 # In[1]:
@@ -45,19 +45,20 @@ full_train_df.head()
 # In[5]:
 
 
-train_df, test_df = train_test_split(
-    full_train_df, test_size=0.1, stratify=full_train_df.target, random_state=42
-)
+train_df, test_df = train_test_split(full_train_df,
+                                     test_size=0.1,
+                                     stratify=full_train_df.target,
+                                     random_state=42)
 
 
 # In[6]:
 
 
 file = os.path.join("data", "train.tsv.gz")
-train_df.to_csv(file, sep="\t", float_format="%.4f", index=False)
+train_df.to_csv(file, sep='\t', float_format="%.4f", index=False)
 
 file = os.path.join("data", "test.tsv.gz")
-test_df.to_csv(file, sep="\t", float_format="%.4f", index=False)
+test_df.to_csv(file, sep='\t', float_format="%.4f", index=False)
 
 
 # In[7]:
@@ -70,13 +71,18 @@ print(test_df.shape)
 # In[8]:
 
 
-target_count_df = pd.DataFrame(train_df.target.value_counts()).merge(
-    pd.DataFrame(test_df.target.value_counts()), left_index=True, right_index=True
+target_count_df = (
+    pd.DataFrame(train_df.target.value_counts()).merge(
+        pd.DataFrame(test_df.target.value_counts()),
+        left_index=True,
+        right_index=True
+    )
 )
 
 target_count_df.columns = ["train_counts", "test_counts"]
 
 file = os.path.join("results", "target_counts.tsv")
-target_count_df.to_csv(file, sep="\t", index=True)
+target_count_df.to_csv(file, sep='\t', index=True)
 
 target_count_df
+

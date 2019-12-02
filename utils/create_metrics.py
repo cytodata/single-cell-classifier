c07fa11908c56dae15af881cbf142af63ba0f958
wagenrace <nijhof.dns@gmail.com>
2019-12-02 18:13:11 +0100

    split to long 1 line for loop into normal for loop

--- utils/create_metrics.py
+++ utils/create_metrics.py
@@ -7,14 +7,17 @@ from .metrics import create_confusion_matrix, add_score2csv
 
 def create_metrics(prediction: list, true_y: list, save_location: str):
     os.makedirs(save_location, exist_ok=True)
-    all_runs = [
-        os.path.join(save_location, o)
-        for o in os.listdir(save_location)
-        if os.path.isdir(os.path.join(save_location, o))
-    ]
+
+    all_runs = []
+    for o in os.listdir(save_location):
+        if os.path.isdir(os.path.join(save_location, o)):
+            all_runs.append(os.path.join(save_location, o))
+
     new_run = str(len(all_runs))
 
-    create_confusion_matrix(prediction, true_y, os.path.join(save_location, new_run))
+    matrix_path = os.path.join(save_location, new_run)
+    create_confusion_matrix(prediction, true_y, matrix_path)
 
+    score_path = os.path.join(save_location, "all_scores.csv")
     score = f1_score(true_y, prediction, average="macro")
-    add_score2csv(score, os.path.join(save_location, "all_scores.csv"))
+    add_score2csv(score, score_path)

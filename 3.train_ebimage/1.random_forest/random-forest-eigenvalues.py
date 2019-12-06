6a2ac20c3fc279dd17a235f6594b3a74472009ce
wagenrace <nijhof.dns@gmail.com>
2019-12-06 08:44:42 +0100

    add random forest eigenvalues

---
+++ 3.train_ebimage/1.random_forest/random-forest-eigenvalues.py
@@ -0,0 +1,39 @@
+# %% Initialization
+import os
+import sys
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+basefolder_loc = Path(__file__).parents[2]
+sys.path.append(str(basefolder_loc))
+
+from utils import create_metrics
+
+data_dir = os.path.join(basefolder_loc, "2.process-data", "data")
+
+
+def load_data(file_name="train_eigen_values.tsv.gz"):
+    file_loc = os.path.join(data_dir, file_name)
+    data = pd.read_csv(file_loc, sep="\t")
+    X = np.array(data.drop(columns=["cell_code", "target"]))
+    Y = np.array(data.target)
+    return X, Y
+
+
+# %% Training model
+RFclf = RandomForestClassifier(n_estimators=100)
+X, Y = load_data("train_eigen_values.tsv.gz")
+RFclf.fit(X, Y)
+
+# %% Validation of model
+valX, valY = load_data("test_eigen_values.tsv.gz")
+prediction = RFclf.predict(valX)
+
+create_metrics(
+    prediction,
+    valY,
+    os.path.join(os.path.join(os.path.dirname(__file__)), "results_eigenvalue"),
+)
